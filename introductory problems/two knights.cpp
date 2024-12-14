bd05a93b5ce3c94d677bad22512d8d8d75e23a11
Nameet <nameetsn@gmail.com>
2024-12-14 11:59:03 +0530

    added folders

---
+++ introductory problems/two knights.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main(){
+  ll n;
+  cin>>n;
+  for(ll i=1;i<=n;i++){
+    ll ans= (i * i * (i * i - 1)) /2 - 4*(i-1)*(i-2);
+    cout<<ans<<endl;
+  }
+}
